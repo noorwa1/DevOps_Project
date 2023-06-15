a839da89d6446366e654db15082faea8c2118258
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2023-06-15 19:43:27 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -2,9 +2,11 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import os
 
+options = webdriver.ChromeOptions()
+options.binary_location = "C:\\Path\\to\\Chrome.exe"
+options.add_argument("chromedriver.exe")  # Path to the chromedriver executable
 
-
-driver = webdriver.Chrome(executable_path="C:\\Users\\noorw\\Downloads\\chromedriver_win32\\chromedriver.exe")
+driver = webdriver.Chrome(options=options)
 driver.get("http://127.0.0.1:5500/users/get_user_name/2")
 
 try:
