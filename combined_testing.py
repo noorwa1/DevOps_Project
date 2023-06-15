87008342436c1f3926201a640b5dd151dd371d4c
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2023-06-15 20:13:05 +0300

    Update combined_testing.py

--- combined_testing.py
+++ combined_testing.py
@@ -2,22 +2,24 @@ from seleniumrequests import Chrome
 import db_connector
 from selenium.webdriver.common.by import By
 webdriver=Chrome()
-POST_response= webdriver.request('POST','http://127.0.0.1:5500/users/9',json={'id':9,'user_name':"engld"})
-GET_response= webdriver.request('GET','http://127.0.0.1:5500/users/9')
+id=10
+POST_response = webdriver.request('POST',f'http://127.0.0.1:5500/users/{id}',json={'id':id,'user_name':"engld"})
+GET_response= webdriver.request('GET',f'http://127.0.0.1:5500/users/{id}')
+print(POST_response.text)
 posted_data=eval(POST_response.text)
-data_from_DB=db_connector.get_all_data(9)
-
+data_from_DB=db_connector.get_all_data(id)
+print(data_from_DB)
 try:
     if GET_response.status_code!=200:
         print("something went wrong code error: {}".format(GET_response))
         raise Exception('test faild')
-    if posted_data['user_added']!=data_from_DB[1] or data_from_DB[0]!=9 :
+    if posted_data['user_added']!=data_from_DB[1] or data_from_DB[0]!=id :
         raise Exception('test faild')
     print("everything went well")
 except :
     print(Exception.with_traceback())
 
-webdriver.get('http://127.0.0.1:5500/users/get_user_name/{}'.format(data_from_DB[0]))
+webdriver.get(f'http://127.0.0.1:5501/users/get_user_name/{data_from_DB[0]}')
 id_element=webdriver.find_element(By.ID,value='name')
 if id_element.text!=data_from_DB[1]:
     raise Exception("test faild")
