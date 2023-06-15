a93d7e6d511dde5d5f2c4b2ff1c9330ed38f9b7c
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2023-06-15 21:06:20 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,14 +1,19 @@
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-
-driver = webdriver.Chrome(service=Service("C:\Users\noorw\Downloads\chromedriver_win32\chromedriver.exe"))
-
+'''
+options = webdriver.ChromeOptions()
+options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe"
+options.add_argument("chromedriver.exe")  # Path to the chromedriver executable
+driver = webdriver.Chrome(options=options)
+print(driver.title)
+'''
+driver = webdriver.Chrome(service=Service("C:\\Users\\noorw\\Downloads\\chromedriver_win32\\chromedriver.exe"))
 driver.get("http://127.0.0.1:5501/users/get_user_name/2")
 
 try:
     id_element=driver.find_element(By.ID,value="name")
-    driver.implicitly_wait(10)
     print(id_element.text)
 
 except Exception as e:
