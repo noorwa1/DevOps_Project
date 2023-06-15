92f75e516c6f220c0554b167272be06fdce8712b
NOORSLAPTOP\noorw <noorwattad11@gmail.com>
2023-06-15 09:21:43 +0300

    asd

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-driver = webdriver.Chrome("C://Users//noorw//OneDrive//Desktop//web drivers//chromedriver_win32 (1)//chromedriver.exe")
-driver.get("http://127.0.0.1:5500/users/get_user_name/1")
+driver = webdriver.Chrome("C:\\Users\\noorw\\OneDrive\\Desktop\\web drivers\\chromedriver.exe")
+driver.get("http://127.0.0.1:5500/users/get_user_name/2")
 
 try:
     id_element=driver.find_element(By.ID,value="name")
