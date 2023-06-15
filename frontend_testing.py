04f0b17c90251a53e49450228279389b5a79e0ac
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2023-06-15 19:33:54 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import os
 
 
-driver = webdriver.Chrome("chromedriver.exe")
+
+driver = webdriver.Chrome("C:\\Users\\noorw\\Downloads\\chromedriver_win32\\chromedriver.exe")
 driver.get("http://127.0.0.1:5500/users/get_user_name/2")
 
 try:
