567630efe64d80109b32dafae54bbd2348e52c68
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2023-06-15 20:42:05 +0300

    Update combined_testing.py

--- combined_testing.py
+++ combined_testing.py
@@ -1,14 +1,15 @@
 from seleniumrequests import Chrome
 import db_connector
 from selenium.webdriver.common.by import By
+from selenium import webdriver as drive
 webdriver=Chrome()
 id=10
+
 POST_response = webdriver.request('POST',f'http://127.0.0.1:5500/users/{id}',json={'id':id,'user_name':"engld"})
 GET_response= webdriver.request('GET',f'http://127.0.0.1:5500/users/{id}')
-print(POST_response.text)
 posted_data=eval(POST_response.text)
 data_from_DB=db_connector.get_all_data(id)
-print(data_from_DB)
+
 try:
     if GET_response.status_code!=200:
         print("something went wrong code error: {}".format(GET_response))
@@ -19,9 +20,17 @@ try:
 except :
     print(Exception.with_traceback())
 
-webdriver.get(f'http://127.0.0.1:5501//users//get_user_name//{data_from_DB[0]}')
-id_element=webdriver.find_element(By.ID,value='name')
+
+
+
+#geting the path of the chrome driver
+options = drive.ChromeOptions()
+options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe"
+options.add_argument("chromedriver.exe")  # Path to the chromedriver executable
+driver = drive.Chrome(options=options)
+
+driver.get(f'http://127.0.0.1:5501//users//get_user_name//{data_from_DB[0]}')
+id_element=driver.find_element(By.ID,value='name')
 print(id_element.text)
 if id_element.text!=data_from_DB[1]:
     raise Exception("test faild")
-
