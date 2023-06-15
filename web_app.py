fe1736ffa8ee1ef3a7ef454c720c11d0a820a14e
NOORSLAPTOP\noorw <noorwattad11@gmail.com>
2023-06-15 08:30:50 +0300

    asdas

--- web_app.py
+++ web_app.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 import pymysql
 import db_connector
 import rest_app
+import os
+import signal
+
 app = Flask(__name__)
 
 @app.route('/users/get_user_name/<user_id>',methods=['GET'])
@@ -12,6 +15,10 @@ def get_user_name(user_id):
         return f'<H1 id="name"> {name[0]} </H1>'
     return f'<H1 id="error">no such user: {user_id} </H1>'
 
+@app.route('/stop_server')
+def stop_server():
+    os.kill(os.getpid(),signal.CTRL_C_EVENT)
+    return 'Server stopped'
 
 
 
